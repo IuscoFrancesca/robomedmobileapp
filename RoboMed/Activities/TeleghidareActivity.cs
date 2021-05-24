952a0408a56d97750686671f3db4eb82435063a3
IuscoFrancesca <iuscofrancesca99@gmail.com>
2021-05-24 23:27:39 +0300

    hello

--- RoboMed/Activities/TeleghidareActivity.cs
+++ RoboMed/Activities/TeleghidareActivity.cs
@@ -16,15 +16,50 @@ namespace RoboMed
     public class TeleghidareActivity : AppCompatActivity
 
     {
+        ImageButton upButton;
+        ImageButton downButton;
+        ImageButton leftButton;
+        ImageButton rightButton;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.teleghidare);
 
+            upButton = FindViewById<ImageButton>(Resource.Id.btnFata);
+            upButton.Click += UpClick;
+
+            downButton = FindViewById<ImageButton>(Resource.Id.btnSpate);
+            downButton.Click += DownClick;
+
+            leftButton = FindViewById<ImageButton>(Resource.Id.btnStanga);
+            leftButton.Click += LeftClick;
+
+            rightButton = FindViewById<ImageButton>(Resource.Id.btnDreapta);
+            rightButton.Click += RightClick;
 
             // Create your application here
         }
 
+        private void RightClick(object sender, EventArgs e)
+        {
+            Toast.MakeText(this, "Dreapta", ToastLength.Long).Show();
+        }
+
+        private void LeftClick(object sender, EventArgs e)
+        {
+            Toast.MakeText(this, "Stanga", ToastLength.Long).Show();
+        }
+
+        private void DownClick(object sender, EventArgs e)
+        {
+            Toast.MakeText(this, "Spate", ToastLength.Long).Show();
+        }
+
+        private void UpClick(object sender, EventArgs e)
+        {
+            Toast.MakeText(this, "Fata", ToastLength.Long).Show();
+        }
     }
 }
